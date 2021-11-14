160703cfe80cdade2f77c81ef97ea99258006f7a
luochao <luochao@joyy.com>
2021-11-14 17:52:15 +0800

    fix: 修复 jest 运行报错

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,17 +3,17 @@
  * @author wangfupeng
  */
 
-// import { createEditor } from '@wangeditor/editor'
-// import withCtrlEnter from '../src/index'
+import { createEditor } from "@wangeditor/editor";
+// import withCtrlEnter from "../src/index";
 
-describe('withCtrlEnter', () => {
-    // it('withCtrlEnter', () => {
-        // const editor = createEditor({ selector: 'body' })
-        // expect(editor).not.toBeNull()
-    // })
+describe("withCtrlEnter", () => {
+    it("withCtrlEnter", () => {
+        const editor = createEditor({ selector: "body" });
+        expect(editor).not.toBeNull();
+    });
 
     // 测试待补充...先用 demo 代替
-    it('demo', () => {
-        expect(1).toBe(1)
-    })
-})
+    it("demo", () => {
+        expect(1).toBe(1);
+    });
+});
